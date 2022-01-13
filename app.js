0d58e030149467db42e4c5eebe9cb46135529720
erikmachado <erikmachadopro@gmail.com>
2022-01-13 13:51:16 -0300

    criação da função gravar() em localStorage

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ function cadastrarDespesa(){
         valor.value
     )
 
-    console.log(despesa)
+    gravar(despesa)
+}
+
+function gravar(d){
+    localStorage.setItem('despesa', JSON.stringify(d))
 }
 
