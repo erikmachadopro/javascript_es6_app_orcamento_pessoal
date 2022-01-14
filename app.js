08fd7bdf85dcc3146c3aeaaa9a187c006a69ca8a
erikmachado <erikmachadopro@gmail.com>
2022-01-14 11:58:25 -0300

    implementando lógica de consulta - carregaListaDespesas() e recuperarTodosRegistros()

--- app.js
+++ app.js
@@ -33,8 +33,30 @@ class Bd{
 
     gravar(d){
         let id = this.getProximoId()
-        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem(id, JSON.stringify(d)) // converter para string
         localStorage.setItem('id', id)
+    }
+
+    recuperarTodosRegistros(){
+
+        //array de despesas
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+        
+        //recuperar todas as despesas cadastradas em localStorage
+        for(let i = 1; i <= id; i++){
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i)) //converter para objeto literal
+
+            //Verificar se existe índices que foram pulados/removidos, nestes casos pular esses índices
+            if (despesa === null) {
+                continue
+            }
+
+            despesas.push(despesa)
+        }
+        return despesas
     }
 }
 
@@ -77,6 +99,12 @@ function cadastrarDespesa(){
         $('#modalRegistraDespesa').modal('show')
         
     }
+}
+
+function carregaListaDespesas(){
+    let despesas = Array()
+    despesas = bd.recuperarTodosRegistros()
+    console.log(despesas)
 }
 
 
